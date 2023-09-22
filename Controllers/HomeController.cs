b1bb41000fb3eaa90f2124bb25bd26c603210b4d
Anderson Damasceno <83956940+Andersonmelodamasceno@users.noreply.github.com>
2023-09-22 14:10:44 -0300

    meu site mvc

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Meusitemvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Meusitemvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            HomeModel home = new HomeModel();
+            home.Nome = "Anderson De Melo Damasceno";
+            home.Email = "dinhomelo_17@hotmail.com";
+            return View(home);
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
